68960285b9e71d98df60e3aeb3cd0b033b3405a0
WebUnica Chile <javier.millar@gmail.com>
2026-04-21 18:01:17 -0400

    fix(build): rename middleware back to proxy.ts and resolve typescript value scope error

--- proxy.ts
+++ proxy.ts
@@ -65,7 +65,7 @@ export async function middleware(request: NextRequest) {
             })
             response.cookies.set({
               name,
-              value,
+              value: '',
               ...options,
             })
           },
