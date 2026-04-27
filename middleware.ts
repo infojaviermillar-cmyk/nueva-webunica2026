1c7feadef76350861fa2e6054fa711199546f944
WebUnica Chile <javier.millar@gmail.com>
2026-04-27 18:46:10 -0400

    fix: rename proxy.ts to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export default async function proxy(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
   // 1. Manejo de dominios para EMD (Desarrollo Shopify)
   const hostname = request.headers.get('host') || '';
   
