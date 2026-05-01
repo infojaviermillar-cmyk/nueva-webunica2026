d66305f28c2d26bd4a1b6d4c346d99b3585283a4
topcasasa <topcasas@webunica.cl>
2026-04-30 20:58:02 -0400

    fix: resolve duplicate CheckCircle2 definition in pricing plans

--- components/sections/pricing-plans.tsx
+++ components/sections/pricing-plans.tsx
@@ -133,23 +133,3 @@ export default function PricingPlans() {
   );
 }
 
-function CheckCircle2({ className, ...props }: any) {
-  return (
-    <svg 
-      {...props} 
-      className={className} 
-      xmlns="http://www.w3.org/2000/svg" 
-      width="24" 
-      height="24" 
-      viewBox="0 0 24 24" 
-      fill="none" 
-      stroke="currentColor" 
-      strokeWidth="3" 
-      strokeLinecap="round" 
-      strokeLinejoin="round"
-    >
-      <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"/>
-      <polyline points="22 4 12 14.01 9 11.01"/>
-    </svg>
-  );
-}
